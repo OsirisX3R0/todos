5fa829689fa34c546e6d6a60b193b3eb4efe7990
Richard Harris <richardharris916@gmail.com>
2021-03-30 21:53:51 -0500

    Successfully implemented Todos struct

--- src/todo_list.rs
+++ src/todo_list.rs
@@ -7,15 +7,15 @@ use crate::todo;
 /// A list of `Todos`
 pub struct TodoList {
   /// The list of todos
-  todos: Vec<todo::Todo>,
+  todos: Todos,
 }
 
 impl PartialEq for TodoList {
   fn eq(&self, other: &Self) -> bool {
     // Start with equal as true
     let mut equal = true;
-    let todo1_len = self.todos.len();
-    let todo2_len = other.todos.len();
+    let todo1_len = self.todos.0.len();
+    let todo2_len = other.todos.0.len();
 
     // If the lengths do not match...
     if todo1_len != todo2_len {
@@ -24,12 +24,12 @@ impl PartialEq for TodoList {
       // If at least one length is zero
       if todo1_len != 0 || todo2_len != 0 {
         // Iterate/enumerate over todos
-        for (i, _x) in self.todos.iter().enumerate() {
+        for (i, _x) in self.todos.0.iter().enumerate() {
           // If last value was equal...
           if equal {
             // Check equality and set equal variable
-            let todo1 = &self.todos[i];
-            let todo2 = &other.todos[i];
+            let todo1 = &self.todos.0[i];
+            let todo2 = &other.todos.0[i];
             equal = todo1.eq(todo2);
           }
         }
@@ -42,7 +42,7 @@ impl PartialEq for TodoList {
 
 impl Display for TodoList {
   fn fmt(&self, f: &mut Formatter) -> Result {
-    self.todos.iter().fold(Ok(()), |result, todo| {
+    self.todos.0.iter().fold(Ok(()), |result, todo| {
       result.and_then(|_| writeln!(f, "{}", todo))
     })
   }
@@ -50,20 +50,23 @@ impl Display for TodoList {
 
 // For listing todos; We can avoid using this with the above implementation
 // MORE TROUBLE THAN IT'S WORTH!!!!!!!!
-// pub struct Todos(pub Vec<todo::Todo>);
+#[derive(Debug)]
+pub struct Todos(pub Vec<todo::Todo>);
 
-// impl Display for Todos {
-//     fn fmt(&self, f: &mut Formatter) -> Result {
-//         self.0.iter().fold(Ok(()), |result, todo| {
-//             result.and_then(|_| writeln!(f, "{}", todo))
-//         })
-//     }
-// }
+impl Display for Todos {
+  fn fmt(&self, f: &mut Formatter) -> Result {
+    self.0.iter().fold(Ok(()), |result, todo| {
+      result.and_then(|_| writeln!(f, "{}", todo))
+    })
+  }
+}
 
 impl TodoList {
   /// Creates a new TodoList
   pub fn new() -> TodoList {
-    TodoList { todos: Vec::new() }
+    TodoList {
+      todos: Todos(Vec::new()),
+    }
   }
 
   /// Lists the todos in the TodoList
@@ -73,41 +76,41 @@ impl TodoList {
 
   /// Get a single todo::Todo by index
   pub fn get(&self, index: usize) -> &todo::Todo {
-    &self.todos[index]
+    &self.todos.0[index]
   }
 
   /// Add a todo to the list
   pub fn add(&mut self, text: &str) {
     let todo: todo::Todo = todo::Todo::new(text);
-    self.todos.push(todo);
+    self.todos.0.push(todo);
   }
 
   /// Update the text of a todo by index
   pub fn update_text(&mut self, index: usize, text: &str) {
-    let todo: &mut todo::Todo = &mut self.todos[index];
+    let todo: &mut todo::Todo = &mut self.todos.0[index];
     todo.update_text(text)
   }
 
   /// Mark a todo as complete by index
   pub fn complete(&mut self, index: usize) {
-    let todo: &mut todo::Todo = &mut self.todos[index];
+    let todo: &mut todo::Todo = &mut self.todos.0[index];
     todo.complete()
   }
 
   /// Mark a todo as incomplete by index
   pub fn incomplete(&mut self, index: usize) {
-    let todo: &mut todo::Todo = &mut self.todos[index];
+    let todo: &mut todo::Todo = &mut self.todos.0[index];
     todo.incomplete()
   }
 
   /// Delete a todo by index
   pub fn delete(&mut self, index: usize) {
-    self.todos.remove(index);
+    self.todos.0.remove(index);
   }
 
   /// Clear all todos
   pub fn clear(&mut self) {
-    self.todos = Vec::new()
+    self.todos = Todos(Vec::new())
   }
 }
 
@@ -118,14 +121,18 @@ impl TodoList {
 #[test]
 fn should_create_new_todo_list() {
   let default_list = TodoList::new();
-  let new_list = TodoList { todos: Vec::new() };
+  let new_list = TodoList {
+    todos: Todos(Vec::new()),
+  };
   assert_eq!(default_list == new_list, true);
 }
 
 #[test]
 fn should_not_be_equal() {
   let mut default_list = TodoList::new();
-  let mut new_list = TodoList { todos: Vec::new() };
+  let mut new_list = TodoList {
+    todos: Todos(Vec::new()),
+  };
   new_list.add("A todo!");
   assert_ne!(default_list == new_list, true);
   default_list.add("A different todo!");
